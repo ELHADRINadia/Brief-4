122ecf36d03226fa7cc86d7b073b4d4e3478a297
Omar Baabouj <74463690+baabouj123@users.noreply.github.com>
2022-01-24 10:19:05 +0100

    fetch.js updated

--- js/fetch.js
+++ js/fetch.js
@@ -27,16 +27,6 @@ export default {
     } catch (error) {
       console.log(error);
     }
-    // fetch(API, {
-    //   method: "POST",
-    //   body: product,
-    //   // headers: {
-    //   //   "Content-Type": "application/json",
-    //   // },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
   },
   update: async (product) => {
     try {
@@ -51,16 +41,6 @@ export default {
     } catch (error) {
       console.log(error);
     }
-    // fetch(API, {
-    //   method: "PUT",
-    //   body: JSON.stringify(product),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
   },
   delete: async (id) => {
     try {
@@ -75,131 +55,6 @@ export default {
     } catch (error) {
       console.log(error);
     }
-
-<<<<<<< HEAD
-                },
-            })
-            .then((res) => res.json())
-            .then((response) => console.log(response))
-            .catch((err) => console.log(err));
-    };
-    const deleteProduct = (product) => {
-        fetch(API, {
-                method: "DELETE",
-                body: JSON.stringify(product),
-                headers: {
-                    "Content-Type": "application/json",
-
-                },
-            })
-            .then((res) => res.json())
-            .then((response) => console.log(response))
-            .catch((err) => console.log(err));
-    };
-    const form = document.querySelector("#add-product-form");
-=======
-    // fetch(API, {
-    //   method: "DELETE",
-    //   body: JSON.stringify({ id }),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
   },
 };
->>>>>>> 5354c4fc8c67ad861e041d4bd74bd019450e89ba
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   const API = "server/api/products.php";
-
-//   const getProducts = () => {
-//     fetch(API)
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const createProduct = (product) => {
-//     fetch(API, {
-//       method: "POST",
-//       body: product,
-//       // headers: {
-//       //   "Content-Type": "application/json",
-//       // },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const updateProduct = (product) => {
-//     fetch(API, {
-//       method: "PUT",
-//       body: JSON.stringify(product),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const deleteProduct = (id) => {
-//     fetch(API, {
-//       method: "DELETE",
-//       body: JSON.stringify({ id }),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-
-<<<<<<< HEAD
-        {
-            id: 2
-        }
-        // createProduct(data);
-        // updateProduct(data);
-        // deleteProduct
-
-    });
-});
-=======
-//   const form = document.querySelector("#add-product-form");
-
-//   form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-
-//     let formData = new FormData();
-//     const name = document.querySelector("#name").value;
-//     const price = document.querySelector("#price").value;
-//     const quantity = document.querySelector("#quantity").value;
-//     // const category = document.querySelector("#category").value;
-//     const category = 1;
-//     const image = document.querySelector("#image").files[0];
-//     const data = {
-//       //   id: 1,
-//       name,
-//       price,
-//       quantity,
-//       category,
-//       image,
-//     };
-
-//     formData.append("name", name);
-//     formData.append("price", price);
-//     formData.append("quantity", quantity);
-//     formData.append("category", category);
-//     formData.append("image", image);
-
-//     // getProducts();
-//     createProduct(formData);
-//     // updateProduct(data);
-//     // deleteProduct(63);
-//   });
-// });
->>>>>>> 5354c4fc8c67ad861e041d4bd74bd019450e89ba
