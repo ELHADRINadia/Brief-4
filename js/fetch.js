f04c3f65ac4a40afe2ed7d5a338849e55be1f2db
unknown <elhadrinadia219@gmail.com>
2022-01-24 14:08:51 +0100

    add employer && moifier

--- js/fetch.js
+++ js/fetch.js
@@ -1,205 +1,59 @@
 const API = "server/api/products.php";
 
 export default {
-  getAll: async () => {
-    try {
-      const response = await fetch(API);
-      return response.json();
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getOne: async (id) => {
-    try {
-      const response = await fetch(`${API}?id=${id}`);
-      return response.json();
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  create: async (product) => {
-    try {
-      const response = await fetch(API, {
-        method: "POST",
-        body: product,
-      });
-      return response.json();
-    } catch (error) {
-      console.log(error);
-    }
-    // fetch(API, {
-    //   method: "POST",
-    //   body: product,
-    //   // headers: {
-    //   //   "Content-Type": "application/json",
-    //   // },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
-  },
-  update: async (product) => {
-    try {
-      const response = await fetch(API, {
-        method: "PUT",
-        body: JSON.stringify(product),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return response.json();
-    } catch (error) {
-      console.log(error);
-    }
-    // fetch(API, {
-    //   method: "PUT",
-    //   body: JSON.stringify(product),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
-  },
-  delete: async (id) => {
-    try {
-      const response = await fetch(API, {
-        method: "DELETE",
-        body: JSON.stringify({ id }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return response.json();
-    } catch (error) {
-      console.log(error);
-    }
-
-<<<<<<< HEAD
-                },
-            })
-            .then((res) => res.json())
-            .then((response) => console.log(response))
-            .catch((err) => console.log(err));
-    };
-    const deleteProduct = (product) => {
-        fetch(API, {
-                method: "DELETE",
+    getAll: async() => {
+        try {
+            const response = await fetch(API);
+            return response.json();
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getOne: async(id) => {
+        try {
+            const response = await fetch(`${API}?id=${id}`);
+            return response.json();
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    create: async(product) => {
+        try {
+            const response = await fetch(API, {
+                method: "POST",
+                body: product,
+            });
+            return response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    update: async(product) => {
+        try {
+            const response = await fetch(API, {
+                method: "PUT",
                 body: JSON.stringify(product),
                 headers: {
                     "Content-Type": "application/json",
-
                 },
-            })
-            .then((res) => res.json())
-            .then((response) => console.log(response))
-            .catch((err) => console.log(err));
-    };
-    const form = document.querySelector("#add-product-form");
-=======
-    // fetch(API, {
-    //   method: "DELETE",
-    //   body: JSON.stringify({ id }),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((res) => res.json())
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
-  },
-};
->>>>>>> 5354c4fc8c67ad861e041d4bd74bd019450e89ba
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   const API = "server/api/products.php";
-
-//   const getProducts = () => {
-//     fetch(API)
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const createProduct = (product) => {
-//     fetch(API, {
-//       method: "POST",
-//       body: product,
-//       // headers: {
-//       //   "Content-Type": "application/json",
-//       // },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const updateProduct = (product) => {
-//     fetch(API, {
-//       method: "PUT",
-//       body: JSON.stringify(product),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-//   const deleteProduct = (id) => {
-//     fetch(API, {
-//       method: "DELETE",
-//       body: JSON.stringify({ id }),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((res) => res.json())
-//       .then((response) => console.log(response))
-//       .catch((err) => console.log(err));
-//   };
-
-<<<<<<< HEAD
-        {
-            id: 2
+            });
+            return response.json();
+        } catch (error) {
+            console.log(error);
         }
-        // createProduct(data);
-        // updateProduct(data);
-        // deleteProduct
-
-    });
-});
-=======
-//   const form = document.querySelector("#add-product-form");
-
-//   form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-
-//     let formData = new FormData();
-//     const name = document.querySelector("#name").value;
-//     const price = document.querySelector("#price").value;
-//     const quantity = document.querySelector("#quantity").value;
-//     // const category = document.querySelector("#category").value;
-//     const category = 1;
-//     const image = document.querySelector("#image").files[0];
-//     const data = {
-//       //   id: 1,
-//       name,
-//       price,
-//       quantity,
-//       category,
-//       image,
-//     };
-
-//     formData.append("name", name);
-//     formData.append("price", price);
-//     formData.append("quantity", quantity);
-//     formData.append("category", category);
-//     formData.append("image", image);
-
-//     // getProducts();
-//     createProduct(formData);
-//     // updateProduct(data);
-//     // deleteProduct(63);
-//   });
-// });
->>>>>>> 5354c4fc8c67ad861e041d4bd74bd019450e89ba
+    },
+    delete: async(id) => {
+        try {
+            const response = await fetch(API, {
+                method: "DELETE",
+                body: JSON.stringify({ id }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            return response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
--- js/navigation.js
+++ js/navigation.js
@@ -1,26 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll("aside .icon");
-  const pages = [
-    "products-page",
-    "statistics-page",
-    "add-product-page",
-    "rapports-page",
-  ];
-  const navigate = (name) => {
-    tabs.forEach((tab) =>
-      tab.id === name
-        ? tab.classList.add("active-tab")
-        : tab.classList.remove("active-tab")
-    );
-    pages.forEach((page) => {
-      page != name
-        ? document
-            .querySelectorAll(`.${page}`)
-            .forEach((p) => (p.style.display = "none"))
-        : document
-            .querySelectorAll(`.${page}`)
-            .forEach((p) => (p.style.display = "flex"));
-    });
-  };
-  tabs.forEach((tab) => (tab.onclick = () => navigate(tab.id)));
+    const tabs = document.querySelectorAll("aside .icon");
+    const pages = [
+        "products-page",
+        "statistics-page",
+        "add-product-page",
+        "add-employee-page",
+    ];
+    const navigate = (name) => {
+        tabs.forEach((tab) =>
+            tab.id === name ?
+            tab.classList.add("active-tab") :
+            tab.classList.remove("active-tab")
+        );
+        pages.forEach((page) => {
+            page != name ?
+                document
+                .querySelectorAll(`.${page}`)
+                .forEach((p) => (p.style.display = "none")) :
+                document
+                .querySelectorAll(`.${page}`)
+                .forEach((p) => (p.style.display = "flex"));
+        });
+    };
+    tabs.forEach((tab) => (tab.onclick = () => navigate(tab.id)));
 });
